4f7dca248803c85e6fbeb3edaf5b4dba4fce58b5
kaunjovi <kaunjovi@gmail.com>
2016-09-25 00:52:22 +0530

    Got the spring boot to run.

--- src/main/java/fun/and/games/MyApplication.java
+++ src/main/java/fun/and/games/MyApplication.java
@@ -2,13 +2,17 @@ package fun.and.games;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MyApplication {
 	
 	final static Logger logger = LoggerFactory.getLogger(MyApplication.class);
 	
 	public static void main (String[] args){
 		logger.debug("Hello world from Spring Boot.");
+		SpringApplication.run(MyApplication.class, args) ; 
 	}
 
 }
